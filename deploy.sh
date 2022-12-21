fda2b777aa02b01413f43c9d0760a37791a2b9fc
Dima Vasilyuk <vasilukwolf@gmail.com>
2022-12-21 21:54:36 +0300

    Hotfix js

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,6 @@
 docker-compose up -d
 docker-compose exec -T app php artisan key:generate
 docker-compose exec -T app composer install
-docker exec app php artisan migrate
+docker exec app -T php artisan migrate
 echo 'Admin token'
-echo '9ac051793bd31855bb6d377cdc9357228f032626a79920dd1da7a6ed8b4211cf'
-echo '/admin/{token}'
+echo 'http://127.0.0.1:8100/admin/9ac051793bd31855bb6d377cdc9357228f032626a79920dd1da7a6ed8b4211cf'
