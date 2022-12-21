3d29f0849561b2ef9ff630a99f5ba64316de88ed
Dima Vasilyuk <vasilukwolf@gmail.com>
2022-12-21 19:22:44 +0300

    Composer install

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,6 @@
 docker-compose up -d
+docker-compose exec -T app php artisan key:generate
+docker-compose exec -T app composer install
 docker exec app php artisan migrate
 echo 'Admin token'
 echo '9ac051793bd31855bb6d377cdc9357228f032626a79920dd1da7a6ed8b4211cf'
